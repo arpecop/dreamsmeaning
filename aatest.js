5d7a8533cfd7c1df1f3e8aa8b3964bd9fe59cc97
Radoslav Georgiev <rudix.lab@gmail.com>
2019-03-29 02:54:30 +0200

    dasd
    dsad

---
+++ aatest.js
@@ -0,0 +1,20 @@
+(function(console) {
+  console.log = function(data, filename) {
+    if (!data) {
+      console.error('Console.save: No data');
+      return;
+    }
+    if (!filename) filename = 'console.json';
+    if (typeof data === 'object') {
+      data = JSON.stringify(data, undefined, 4);
+    }
+    const blob = new Blob([data], { type: 'text/json' });
+    const e = document.createEvent('MouseEvents');
+    const a = document.createElement('a');
+    a.download = filename;
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+    console.warn(a);
+  };
+})(console);
+alert(1)
