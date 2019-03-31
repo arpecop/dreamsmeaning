cd2d45ae2ecdc05d32c9218daa58ccf7f64226c3
Radoslav Georgiev <rudix.lab@gmail.com>
2019-04-01 01:24:06 +0300

    sss
    sss

--- aadebugger.js
+++ aadebugger.js
@@ -1,22 +1,7 @@
-
-    var console = {};
-    var key = 0;
- console.log = function(log) {
-  window.postMessage( JSON.stringify({log,type:typeof(log),line:true,key:key.toString(),odd:key % 2 == 0}));
-  key++;
- };
- console.debug = console.log;
- console.info = console.log;
- console.warn = console.log;
- console.error = console.log;
- window.console = console;
-
 window.onerror = function (message, file, line, col, error) {
-  
   return false;
 };
-window.addEventListener('error', function(event) {
-
-  console.log(event);
+window.addEventListener("error", function (e) {
+  console.log(e.error.Message);
+  return false;
 })
- 
