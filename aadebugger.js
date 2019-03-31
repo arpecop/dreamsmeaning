4e1be7ac765bb3cdb033b87b060b44417a28d4c4
Radoslav Georgiev <rudix.lab@gmail.com>
2019-03-31 20:54:21 +0300

    dasd
    asdasd

--- aadebugger.js
+++ aadebugger.js
@@ -1,7 +1,8 @@
 window.onerror = function (message, file, line, col, error) {
+    console.log(message, file, line, col, error);
   return false;
 };
 window.addEventListener("error", function (e) {
-  console.log(e.error.Message);
+  console.log(e.error.message);
   return false;
 })
