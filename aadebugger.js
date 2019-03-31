909a981d3e918ee8b4b9dfc978f2aabc315e1ae7
Radoslav Georgiev <rudix.lab@gmail.com>
2019-03-31 21:09:17 +0300

    dasd
    asdasd

--- aadebugger.js
+++ aadebugger.js
@@ -1,3 +1,16 @@
+
+    var console = {};
+    var key = 0;
+ console.log = function(log) {
+  window.postMessage( JSON.stringify({log,type:typeof(log),line:true,key:key.toString(),odd:key % 2 == 0}));
+  key++;
+ };
+ console.debug = console.log;
+ console.info = console.log;
+ console.warn = console.log;
+ console.error = console.log;
+ window.console = console;
+
 window.onerror = function (message, file, line, col, error) {
   
   return false;
